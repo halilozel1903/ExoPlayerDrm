09b73686b2f83a278b1328acda889b9567e5a0f2
halilozel1903 <halilozel1903@gmail.com>
2022-03-20 14:19:21 +0300

    ExoPlayer settings was made for DRM.

--- app/src/main/java/com/halil/ozel/exoplayerdrm/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/exoplayerdrm/MainActivity.kt
@@ -1,58 +1,48 @@
 package com.halil.ozel.exoplayerdrm
 
 import android.app.Activity
-import android.content.Context
 import android.net.Uri
 import android.os.Bundle
 import com.google.android.exoplayer2.C
-import com.google.android.exoplayer2.DefaultLoadControl
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
-import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.dash.DashChunkSource
 import com.google.android.exoplayer2.source.dash.DashMediaSource
 import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource
-import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
-import com.google.android.exoplayer2.ui.PlayerView
 import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter
 import com.google.android.exoplayer2.upstream.DefaultHttpDataSource
 import com.google.android.exoplayer2.util.MimeTypes
+import com.halil.ozel.exoplayerdrm.databinding.ActivityMainBinding
 
-// DRM URL : https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/mpds/11331.mpd
-// NON DRM URL : https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd
+/** DRM URL : https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/mpds/11331.mpd **/
+/** NON DRM URL : https://bitmovin-a.akamaihd.net/content/MI201109210084_1/mpds/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.mpd **/
 
 class MainActivity : Activity() {
 
-    private lateinit var playerView: PlayerView
+    private lateinit var playerView: ExoPlayer
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        playerView = findViewById(R.id.playerView)
-
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         initializePlayer()
     }
 
-    private val context: Context
-        get() = this.applicationContext as Context
 
     private fun initializePlayer() {
 
         val url = "https://bitmovin-a.akamaihd.net/content/art-of-motion_drm/mpds/11331.mpd"
         val drmLicenseUrl = "https://proxy.uat.widevine.com/proxy?provider=widevine_test"
-        val drmSchemeUuid = C.WIDEVINE_UUID
+        val drmSchemeUuid = C.WIDEVINE_UUID // DRM Type
         val userAgent = "ExoPlayer-Drm"
-//        val userAgent = "userAgent"
-
-        val trackSelector = DefaultTrackSelector(context)
-        trackSelector.setParameters(
-            trackSelector.buildUponParameters()
-                .setMaxVideoSize(200, 200)
-        )
+        // val userAgent = "userAgent"
 
         val defaultHttpDataSourceFactory = DefaultHttpDataSource.Factory()
             .setUserAgent(userAgent)
             .setTransferListener(
-                DefaultBandwidthMeter.Builder(context)
+                DefaultBandwidthMeter.Builder(this)
                     .setResetOnNetworkTypeChange(false)
                     .build()
             )
@@ -61,38 +51,35 @@ class MainActivity : Activity() {
             defaultHttpDataSourceFactory
         )
         val manifestDataSourceFactory = DefaultHttpDataSource.Factory().setUserAgent(userAgent)
-        val dashMediaSource = DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory)
-            .createMediaSource(
-                MediaItem.Builder()
-                    .setUri(Uri.parse(url))
-                    .setDrmUuid(drmSchemeUuid)
-                    .setDrmMultiSession(true)
-                    .setDrmLicenseUri(drmLicenseUrl)
-                    .setMimeType(MimeTypes.APPLICATION_MPD)
-                    .setTag(null)
-                    .build()
-            )
+        val dashMediaSource =
+            DashMediaSource.Factory(dashChunkSourceFactory, manifestDataSourceFactory)
+                .createMediaSource(
+                    MediaItem.Builder()
+                        .setUri(Uri.parse(url))
+                        // DRM Configuration
+                        .setDrmConfiguration(
+                            MediaItem.DrmConfiguration.Builder(drmSchemeUuid)
+                                .setLicenseUri(drmLicenseUrl).build()
+                        )
+                        .setMimeType(MimeTypes.APPLICATION_MPD)
+                        .setTag(null)
+                        .build()
+                )
 
-        val defaultLoadControl = DefaultLoadControl()
-        val player: SimpleExoPlayer = SimpleExoPlayer.Builder(context)
-            .setLoadControl(defaultLoadControl)
-            .setTrackSelector(trackSelector)
-            .build()
-
-        // Bind the player to the view.
-        playerView.player = player
-
-        player.playWhenReady = true
-
-        // Build the media item.
 
         // Prepare the player.
-        player.setMediaSource(dashMediaSource, true)
-        player.prepare()
+        playerView = ExoPlayer.Builder(this)
+            .setSeekForwardIncrementMs(10000)
+            .setSeekBackIncrementMs(10000)
+            .build()
+        playerView.playWhenReady = true
+        binding.playerView.player = playerView
+        playerView.setMediaSource(dashMediaSource, true)
+        playerView.prepare()
     }
 
     override fun onPause() {
         super.onPause()
-        playerView.player?.playWhenReady = false
+        playerView.playWhenReady = false
     }
 }
